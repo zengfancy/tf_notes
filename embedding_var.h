d2286e38d2f199ee2ea68db58cc5db51fabd6614
Fanxi Zeng <zengfancy@126.com>
2019-04-08 13:57:47 +0800

    modified

--- embedding_var.h
+++ embedding_var.h
@@ -4,6 +4,7 @@ class EmbeddingVar : public ResourceBase {
 public:
   virtual void GetEmbedding(K key, V** data) = 0;
   virtual void PutEmbedding(K key, V* data, scatter_op::UpdateOp op) = 0;
+  
   TensorShape GetValueShape();
 };
 
@@ -11,6 +12,16 @@ template <typename K, typename V>
 class HashEmbeddingVar : public EmbeddingVar<K, V> {
 
 private:
-  
+  std::unordered_map<K, PersistentTensor> tensor_map_;
 };
 
+template <typename K, typename V>
+class DenseEmbeddingVar2 : public EmbeddingVar<K, V> {
+public:
+  
+private:
+  PersistentTensor first_keys_;
+  PersistentTensor keys_;
+  PersistentTensor values_;
+  PersistentTensor next_;
+};
